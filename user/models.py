9dee492205458452dff206323679eaf3b4657b4b
sulthanftr <sulthanftr@gmail.com>
2024-05-23 13:35:14 +0700

    refactor: fix user model default values

--- user/models.py
+++ user/models.py
@@ -4,5 +4,5 @@ from django.db import models
 
 class CustomUser(AbstractUser):
     bio = models.CharField(max_length=255, blank=True)
-    follower_count = models.IntegerField()
-    following_count = models.IntegerField()
+    follower_count = models.IntegerField(default=0)
+    following_count = models.IntegerField(default=0)
