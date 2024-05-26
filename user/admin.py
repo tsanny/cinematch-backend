50a7893cf5bc391992e2af397ddc7702889c7ca4
sulthanftr <sulthanftr@gmail.com>
2024-05-26 12:16:16 +0700

    feat: model for user following

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import CustomUser, UserFollowing
 
 # Register your models here.
+admin.site.register(CustomUser)
+admin.site.register(UserFollowing)
--- user/models.py
+++ user/models.py
@@ -6,3 +6,16 @@ class CustomUser(AbstractUser):
     bio = models.CharField(max_length=255, blank=True)
     follower_count = models.IntegerField(default=0)
     following_count = models.IntegerField(default=0)
+
+
+class UserFollowing(models.Model):
+    user_id = models.ForeignKey("CustomUser",
+                                related_name="following",
+                                on_delete=models.CASCADE)
+    following_user_id = models.ForeignKey("CustomUser",
+                                          related_name="followers",
+                                          on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = [["user_id", "following_user_id"]]
